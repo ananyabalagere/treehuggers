6a2ff1e00175a87c6e3088cbdd320900f2113855
taomis <87916443+taomis@users.noreply.github.com>
2023-11-10 20:21:46 -0800

    idk what im doing

--- page-2/src/App.svelte
+++ page-2/src/App.svelte
@@ -1,47 +1,44 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from 'svelte';
+  import firebase from 'firebase/compat/app';
+  import 'firebase/compat/firestore';
+
+  // Initialize Firebase
+  const firebaseConfig = {
+    // Your Firebase configuration here
+  };
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  const db = firebase.firestore();
+
+  let images = [];
+  let index = 0;
+
+  onMount(async () => {
+    const querySnapshot = await db.collection("images").get();
+    querySnapshot.forEach((doc) => {
+      images.push(doc.data());
+    });
+  });
+
+  const nextImage = () => {
+    index = (index + 1) % images.length;
+  };
+
+  const incrementClicksAndNextImage = async () => {
+    const docRef = db.collection("images").doc(images[index].id);
+    await docRef.update({
+      CLICKS: firebase.firestore.FieldValue.increment(1)
+    });
+    nextImage();
+  };
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  {#if images.length > 0}
+    <img src={images[index].IMG} alt="Image" />
+    <button on:click={nextImage}>Next</button>
+    <button on:click={incrementClicksAndNextImage}>Like and Next</button>
+  {/if}
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>

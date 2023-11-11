6e906fb006f65d7501592de8b1579ee00fec02be
taomis <87916443+taomis@users.noreply.github.com>
2023-11-10 22:07:28 -0800

    fixed 1/3 but not done

--- page-2/src/App.svelte
+++ page-2/src/App.svelte
@@ -1,44 +1,11 @@
 <script>
-  import { onMount } from 'svelte';
-  import firebase from 'firebase/compat/app';
-  import 'firebase/compat/firestore';
-
-  // Initialize Firebase
-  const firebaseConfig = {
-    // Your Firebase configuration here
-  };
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-  const db = firebase.firestore();
-
-  let images = [];
-  let index = 0;
-
-  onMount(async () => {
-    const querySnapshot = await db.collection("images").get();
-    querySnapshot.forEach((doc) => {
-      images.push(doc.data());
-    });
-  });
-
-  const nextImage = () => {
-    index = (index + 1) % images.length;
-  };
-
-  const incrementClicksAndNextImage = async () => {
-    const docRef = db.collection("images").doc(images[index].id);
-    await docRef.update({
-      CLICKS: firebase.firestore.FieldValue.increment(1)
-    });
-    nextImage();
-  };
+    import UCSC from './lib/UCSC.svelte';
 </script>
 
+<style>
+
+</style>
+
 <main>
-  {#if images.length > 0}
-    <img src={images[index].IMG} alt="Image" />
-    <button on:click={nextImage}>Next</button>
-    <button on:click={incrementClicksAndNextImage}>Like and Next</button>
-  {/if}
+    <UCSC />
 </main>
